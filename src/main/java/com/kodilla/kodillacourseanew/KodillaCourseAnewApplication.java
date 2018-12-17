52d2444fd1aeb4db87924436584bf143ce3393ac
Karolina Lubieniecka <lubieniecka@interia.pl>
2018-12-17 10:03:17 +0100

    Version with gradle issue

---
+++ src/main/java/com/kodilla/kodillacourseanew/KodillaCourseAnewApplication.java
@@ -0,0 +1,14 @@
+package com.kodilla.kodillacourseanew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodillaCourseAnewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KodillaCourseAnewApplication.class, args);
+    }
+
+}
+
